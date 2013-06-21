d62e56a8f93788993b5ef09dd3b893362402d679
carlo.scarioni <carlo.scarioni@gamesys.co.uk>
2013-06-21 19:01:42 +0100

    redirect to login when Ajax request fails because logged out and incresed session expiration to 5 hours

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,6 +1,10 @@
 class AuthenticatedController < ApplicationController
   before_filter :authenticate_google_user!
   def authenticate_google_user!
-    redirect_to '/auth/google_oauth2' if !current_user
+    if request.xhr?
+      render :js => 'window.location = "/auth/google_oauth2"'  if !current_user
+    else
+      redirect_to '/auth/google_oauth2' if !current_user
+    end    
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Muser::Application.config.session_store :cookie_store, key: '_muser_session'
+Muser::Application.config.session_store :cookie_store, key: '_muser_session', expire_after: 5.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
