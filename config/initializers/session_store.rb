c29863b6812f60f64ceae989c8ed74b732beae11
Carlo Scarioni <carlo.scarioni@gmail.com>
2013-09-10 13:05:56 +0100

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Muser::Application.config.session_store :cookie_store, key: '_muser_session', expire_after: 5.hours
+Muser::Application.config.session_store :cookie_store, key: '_muser_session', expire_after: 0
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
